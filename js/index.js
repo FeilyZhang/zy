ac7e8995cfe396735eb8a7ffe4ddbb50048b02c7
Feily Zhang <fei472625@foxmail.com>
2022-10-08 16:05:19 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,81 @@
+
+function convert(num){
+  var bin = "";
+  var conv = [];
+
+  while(num>0) {
+      bin = num%2 + bin;
+      num = Math.floor(num/2);
+  }
+  conv = bin.split("");
+  while(conv.length < 6){
+	  conv.unshift("0");
+  }
+  return conv;
+}
+
+window.setInterval(function(){
+    var since = 1600790220000;
+    var now = new Date().getTime();
+    var d = parseInt((now - since) / 86400000);
+    var remain = (now - since) % 86400000;
+    var h = parseInt(remain / 3600000);
+	remain = remain % 3600000;
+    var m = parseInt(remain / 60000);
+    var s = parseInt((remain % 60000) / 1000);
+  
+    var time= addZero(h) + " : " + addZero(m) + " : " + addZero(s);
+  
+    /*$('.title').text("We've been together for " + d + " days, " + h + " hours, " + m + " minutes, " + s + " seconds.");*/
+	$(".seconds").attr('data-attr', s + " seconds");
+	$(".minutes").attr('data-attr', m + " minutes");
+	$(".hours").attr('data-attr', d + " ds, " + h + " hs");
+    var binaryArray = convert(s);
+    lightSeconds(binaryArray);
+    
+    var binaryArray = convert(m);
+    lightMinutes(binaryArray);
+  
+     var binaryArray = convert(h);
+    lightHours(binaryArray);
+  
+}, 1000);
+
+function lightSeconds(array){
+    $('.seconds td').attr('class','num0');
+  for(var i =0; i<array.length; i++){
+    $('.seconds td:eq('+i+')').attr('class','num'+array[i]);
+  }
+}
+
+function lightMinutes(array){
+    $('.minutes td').attr('class','num0');
+  for(var i =0; i<array.length; i++){
+    $('.minutes td:eq('+i+')').attr('class','num'+array[i]);
+  }
+}
+
+function lightHours(array){
+    $('.hours td').attr('class','num0');
+  for(var i =0; i<array.length; i++){
+    $('.hours td:eq('+i+')').attr('class','num'+array[i]);
+  }
+}
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+$(document).ready(function() {
+  $.ajax({
+    type:"GET",
+    url:"https://v1.jinrishici.com/shuqing/aiqing",
+    dataType:"json",
+    success:function(data){
+      $(".sub-title").text(data.content);
+    }
+  })
+});
